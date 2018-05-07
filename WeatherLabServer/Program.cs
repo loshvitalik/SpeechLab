268911ab3643ac6e1c3c1779d21afe33bb14ca30
MrBlacktop <blacktop7372@gmail.com>
2018-05-07 17:01:16 +0500

    New classes

--- WeatherLabServer/Program.cs
+++ WeatherLabServer/Program.cs
@@ -38,18 +38,9 @@ namespace WeatherLabServer
 			waveSource.StopRecording();
 			Speech = stream.ToArray();
 
-			// speech recognition
+            // speech recognition
 
-			var speech = SpeechClient.Create();
-			var response = speech.Recognize(new RecognitionConfig
-			{
-				Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
-				SampleRateHertz = 44100,
-				LanguageCode = "ru-RU"
-			}, RecognitionAudio.FromBytes(Speech));
-			string result = (response.Results.Count == 0)
-				? "Error"
-				: response.Results[0].Alternatives[0].Transcript;
+		    var result = SpeechRecognition.Recognise(Speech);
 			Console.WriteLine(result);
 		}
 
---
+++ WeatherLabServer/SpeechRecognition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Google.Cloud.Speech.V1;
+
+namespace WeatherLabServer
+{
+    class SpeechRecognition
+    {
+        public static String Recognise(byte[] Speech)
+        {
+            var speech = SpeechClient.Create();
+            var response = speech.Recognize(new RecognitionConfig
+            {
+                Encoding = RecognitionConfig.Types.AudioEncoding.Linear16,
+                SampleRateHertz = 44100,
+                LanguageCode = "ru-RU"
+            }, RecognitionAudio.FromBytes(Speech));
+            string result = (response.Results.Count == 0)
+                ? "Error"
+                : response.Results[0].Alternatives[0].Transcript;
+            return result;
+        }
+    }
+}
