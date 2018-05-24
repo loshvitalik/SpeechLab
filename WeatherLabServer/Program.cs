3199d89f52ef5176ba6ed2da21d173bcd998e563
MrBlacktop <blacktop7372@gmail.com>
2018-05-24 19:06:30 +0500

    Тестирую сервер

--- WeatherLabServer/Program.cs
+++ WeatherLabServer/Program.cs
@@ -12,57 +12,12 @@ namespace WeatherLabServer
 		// на сервере должны остаться только последние строчки Main, с var speech до string result
 		// NAudio нужно будет удалить с сервера!
 
-		static WaveInEvent waveSource;
-		static bool IsOver;
-		static byte[] Speech;
-		static readonly MemoryStream stream = new MemoryStream();
 
 		static void Main(string[] args)
 		{
-			// record sound
+		    var server = new Server();
+            server.Start();
 
-			waveSource = new WaveInEvent
-			{
-				WaveFormat = new WaveFormat(44100, 1)
-			};
-
-			waveSource.DataAvailable += waveSource_DataAvailable;
-			waveSource.RecordingStopped += waveSource_RecordingStopped;
-
-			waveSource.StartRecording();
-
-			var thread = new Thread(Delay);
-			thread.Start();
-			while (!IsOver)
-				Thread.Sleep(10);
-			waveSource.StopRecording();
-			Speech = stream.ToArray();
-
-            // speech recognition
-
-		    var result = SpeechRecognizer.Recognise(Speech);
-			Console.WriteLine(result);
-		}
-
-		static void Delay()
-		{
-			Thread.Sleep(5000);
-			IsOver = true;
-		}
-
-		static void waveSource_DataAvailable(object sender, WaveInEventArgs e)
-		{
-			stream.Write(e.Buffer, 0, e.BytesRecorded);
-			stream.Flush();
-		}
-
-		static void waveSource_RecordingStopped(object sender, StoppedEventArgs e)
-		{
-			if (waveSource != null)
-			{
-				waveSource.Dispose();
-				waveSource = null;
-			}
-		}
+		}	
 	}
 }
--- WeatherLabServer/Server.cs
+++ WeatherLabServer/Server.cs
@@ -11,18 +11,48 @@ namespace WeatherLabServer
 {
     class Server
     {
-        private RouterSocket server;
+        private RouterSocket server;//10.97.129.111
 
-        Server()
+        public Server()
         {
-            server = new RouterSocket("@tcp://127.0.0.1:5556");
+            server = new RouterSocket("@tcp://10.97.129.111:2228");
+
         }
 
         public void Start()
         {
             while (true)
             {
-                var clientMessehe = server.ReceiveMultipartMessage();
+                var clientMessage = server.ReceiveMultipartMessage();
+                PrintFrames("Server reciving",clientMessage);
+                //foreach (var b in messege)
+                //{
+                //    Console.WriteLine(b);
+                //}
+                //Console.WriteLine(messege);
+                //server.SendMultipartBytes(new byte[5]);
+                if (clientMessage.FrameCount == 3)
+                {
+                    var clientAddress = clientMessage[0];
+                    var clientOriginalMessage = clientMessage[2].ConvertToString();
+                    string response = string.Format("{0} back from server {1}",
+                        clientOriginalMessage, DateTime.Now.ToLongTimeString());
+                    var messageToClient = new NetMQMessage();
+                    messageToClient.Append(clientAddress);
+                    messageToClient.AppendEmptyFrame();
+                    messageToClient.Append(response);
+                    server.SendMultipartMessage(messageToClient);
+                }
+
+            }
+     
+        }
+        void PrintFrames(string operationType, NetMQMessage message)
+        {
+            for (int i = 0; i < message.FrameCount; i++)
+            {
+                Console.WriteLine("{0} Socket : Frame[{1}] = {2}", operationType, i,
+                    message[i].ConvertToString());
             }
         }
     }
