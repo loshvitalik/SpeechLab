d95945bb67eb50ebbc0c1ad2a625b9ea88d94fb5
MrBlacktop <blacktop7372@gmail.com>
2018-05-24 20:16:22 +0500

    Добавил лог в консоль

--- WeatherLabServer/Program.cs
+++ WeatherLabServer/Program.cs
@@ -1,23 +1,11 @@
-﻿using System;
-using System.IO;
-using System.Threading;
-using Google.Cloud.Speech.V1;
-using NAudio.Wave;
-
-namespace WeatherLabServer
+﻿namespace WeatherLabServer
 {
-	class Program
-	{
-		// просто пример записи звука и распознавания. Это должно быть раздельно клиент и сервер
-		// на сервере должны остаться только последние строчки Main, с var speech до string result
-		// NAudio нужно будет удалить с сервера!
-
-
-		static void Main(string[] args)
-		{
-		    var server = new Server();
-            server.Start();
-
-		}	
-	}
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var server = new Server();
+            server.Run();
+        }
+    }
 }
--- WeatherLabServer/Server.cs
+++ WeatherLabServer/Server.cs
@@ -13,11 +13,12 @@ namespace WeatherLabServer
             server = new RouterSocket("@tcp://10.97.129.111:2228");
         }
 
-        public void Start()
+        public void Run()
         {
             while (true)
             {
                 var clientMessage = server.ReceiveMultipartMessage();
+                Console.WriteLine("Received a message at " + DateTime.Now.ToLongTimeString());
                 PrintFrames("Server reciving", clientMessage);
                 var clientAddress = clientMessage[0];
                 var messageToClient = new NetMQMessage();
@@ -25,10 +26,12 @@ namespace WeatherLabServer
                 messageToClient.Append("Head: text");
                 messageToClient.Append("Some text");
                 server.SendMultipartMessage(messageToClient);
+                Console.WriteLine("Send a message at " + DateTime.Now.ToLongTimeString());
+                Console.WriteLine();
             }
         }
 
-        private void PrintFrames(string operationType, NetMQMessage message)
+        private void PrintFrames(string operationType, NetMQMessage message) // Test
         {
             for (var i = 0; i < message.FrameCount; i++)
                 Console.WriteLine("{0} Socket : Frame[{1}] = {2}", operationType, i,
