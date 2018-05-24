fc5dd56b477a4a06f19a491bb96a9be37f123179
MrBlacktop <blacktop7372@gmail.com>
2018-05-24 20:39:50 +0500

    Добавил класс для обмена сообщениями в клиент

---
+++ WeatherLab/Dealer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherLab
+{
+    static class Dealer
+    {
+        public static void Recognise(byte[] voice)
+        {
+            var client = new DealerSocket();
+            client.Connect("tcp://10.97.129.111:2228");
+
+        }
+    }
+}
