23a5a0aa23967b2883bba6d0e791ec781c7a4815
MrBlacktop <blacktop7372@gmail.com>
2018-05-24 19:59:44 +0500

    Сервер готов

--- WeatherLabServer/Server.cs
+++ WeatherLabServer/Server.cs
@@ -1,22 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using NetMQ;
 using NetMQ.Sockets;
 
 namespace WeatherLabServer
 {
-    class Server
+    internal class Server
     {
-        private RouterSocket server;//10.97.129.111
+        private readonly RouterSocket server;
 
         public Server()
         {
             server = new RouterSocket("@tcp://10.97.129.111:2228");
-
         }
 
         public void Start()
@@ -24,36 +18,21 @@ namespace WeatherLabServer
             while (true)
             {
                 var clientMessage = server.ReceiveMultipartMessage();
-                PrintFrames("Server reciving",clientMessage);
-                //foreach (var b in messege)
-                //{
-                //    Console.WriteLine(b);
-                //}
-                //Console.WriteLine(messege);
-                //server.SendMultipartBytes(new byte[5]);
-                if (clientMessage.FrameCount == 3)
-                {
-                    var clientAddress = clientMessage[0];
-                    var clientOriginalMessage = clientMessage[2].ConvertToString();
-                    string response = string.Format("{0} back from server {1}",
-                        clientOriginalMessage, DateTime.Now.ToLongTimeString());
-                    var messageToClient = new NetMQMessage();
-                    messageToClient.Append(clientAddress);
-                    messageToClient.AppendEmptyFrame();
-                    messageToClient.Append(response);
-                    server.SendMultipartMessage(messageToClient);
-                }
-
+                PrintFrames("Server reciving", clientMessage);
+                var clientAddress = clientMessage[0];
+                var messageToClient = new NetMQMessage();
+                messageToClient.Append(clientAddress);
+                messageToClient.Append("Head: text");
+                messageToClient.Append("Some text");
+                server.SendMultipartMessage(messageToClient);
             }
-     
         }
-        void PrintFrames(string operationType, NetMQMessage message)
+
+        private void PrintFrames(string operationType, NetMQMessage message)
         {
-            for (int i = 0; i < message.FrameCount; i++)
-            {
+            for (var i = 0; i < message.FrameCount; i++)
                 Console.WriteLine("{0} Socket : Frame[{1}] = {2}", operationType, i,
                     message[i].ConvertToString());
-            }
         }
     }
 }
